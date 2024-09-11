5b2a1c1e57ee0295ef18a7bcb833ee91e4c69de0
david-salac <info@davidsalac.eu>
2024-09-11 08:39:53 +0100

    New create statements

--- default-data/create_statements.sql
+++ default-data/create_statements.sql
@@ -35,3 +35,83 @@ CREATE TABLE GPMedication (
     CourseLength INT,
     FOREIGN KEY (PatientID) REFERENCES PatientDemographics(PatientID)
 );
+
+CREATE TABLE e3_baseline (
+    PatID SERIAL PRIMARY KEY,
+    Sex INT,
+    DateOfSurgery DATE,
+    Eligible INT,
+    TumourType INT,
+    TumourTypeSpecify VARCHAR(100),
+    TumourSize1 INT,
+    TumourSize2 INT,
+    LymphNodes INT,
+    LymphNodeSpecify VARCHAR(50),
+    NumberPositive1 INT,
+    NumberPositive2 INT,
+    ResectionMargins INT,
+    RStatus INT,
+    TNMStagepT INT,
+    TNMStagepN INT,
+    TNMStageM INT,
+    Stage INT,
+    WHOPerfStatus INT,
+    QualityLifeStudy INT,
+    RandTo INT,
+    Differentiation INT,
+    Country INT
+);
+
+CREATE TABLE e3_fu (
+    PatID INT NOT NULL,
+    DateOfAssessment DATE,
+    Weight NUMERIC(4,1),
+    WHOPS INT,
+    Diabetic INT,
+    EnzymeSupp INT,
+    CA199 INT,
+    CA199Date DATE,
+    FOREIGN KEY (PatID) REFERENCES e3_baseline(PatID)
+);
+
+CREATE TABLE e3_nod (
+    PatID INT NOT NULL,
+    DiedDate DATE,
+    DiedTime TIMESTAMP,
+    CauseOfDeath INT,
+    CauseOfDeathSpecify VARCHAR(100),
+    CreatedDate DATE,
+    LastEditDate DATE,
+    Timestamp TIMESTAMP,
+    CensorField DATE,
+    Patinelreason VARCHAR(50),
+    FOREIGN KEY (PatID) REFERENCES e3_baseline(PatID)
+);
+
+CREATE TABLE e3_surg (
+    PatID INT NOT NULL,
+    Height INT,
+    Weight NUMERIC(4,1),
+    Smoking INT,
+    ConMedCond INT,
+    CourseLength INT,
+    ReqTreatment INT,
+    Diabetic INT,
+    EnzymeSupp INT,
+    ASAClass INT,
+    PreOpCA199 INT,
+    PreOpDate DATE,
+    PostOpCA199 INT,
+    PostOpDate DATE,
+    Surgery INT,
+    ExtentOfResection INT,
+    PortalVeinResection INT,
+    Cholecystectomy INT,
+    TruncalVagotomy INT,
+    LiverMetastases INT,
+    Ascites INT,
+    PeritonealMetastases INT,
+    LocalInvasion INT,
+    Diff TEXT,
+    FOREIGN KEY (PatID) REFERENCES e3_baseline(PatID)
+);
